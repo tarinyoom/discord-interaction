dfb7e56fb1a52a80e96d98b8a7b4ebf3477daaa3
Adam Reynolds <adam.chiu.reynolds@gmail.com>
2023-10-22 00:32:52 +0000

    adds ping support for handler

--- src/handler.rs
+++ src/handler.rs
@@ -1,9 +1,13 @@
 use crate::{Request, Response};
+use crate::interaction_types::InteractionType;
 
 pub struct InteractionHandler {}
 
 impl InteractionHandler {
-    pub fn handle_interaction(&self, _: &Request) -> Response {
-        Response::message().content("mega doo doo").into()
+    pub fn handle_interaction(&self, req: &Request) -> Response {
+        match &req.r#type {
+            InteractionType::Ping => Response::pong(),
+            _ => Response::message().content("hello world").into()
+        }
     }
 }
