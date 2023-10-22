8b37dbd0bbd6da7f368eed5b07ec225b082b26d0
Adam Reynolds <adam.chiu.reynolds@gmail.com>
2023-10-22 01:13:36 +0000

    deploy script adds commands

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,10 +1,21 @@
+use discord_interaction::{run_handler, InteractionHandler, InteractionType, Request, Response};
 use lambda_http::Error;
-use discord_interaction::{run_handler, InteractionHandler};
 
 const APPLICATION_PUBLIC_KEY: &str = env!("DEMO_PUBLIC_KEY");
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let handler = InteractionHandler{};
+    let handler = DemoHandler {};
     run_handler(APPLICATION_PUBLIC_KEY, &handler).await
+}
+
+struct DemoHandler;
+
+impl InteractionHandler for DemoHandler {
+    fn handle_interaction(&self, req: &Request) -> Response {
+        match &req.r#type {
+            InteractionType::Ping => Response::pong(),
+            _ => Response::message().content("hello world").into(),
+        }
+    }
 }
